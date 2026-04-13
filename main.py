91d07d759c52d1580ad7559b646a71d8577f2a3e
Omar <mario-omar.ibra@tu-dortmund.de>
2026-04-13 23:53:44 +0200

    empfehlung von claude

--- main.py
+++ main.py
@@ -139,7 +139,12 @@ class ChatHandler(BaseHTTPRequestHandler):
         self.wfile.write(body)
 
     def do_OPTIONS(self):
-        self._send_json(204, {})
+        self.send_response(204)
+        self.send_header("Access-Control-Allow-Origin", "*")
+        self.send_header("Access-Control-Allow-Headers", "Content-Type")
+        self.send_header("Access-Control-Allow-Methods", "POST, OPTIONS, GET")
+        self.send_header("Content-Length", "0")
+        self.end_headers()
 
     def do_GET(self):
         if self.path == "/api/debug-stores":
